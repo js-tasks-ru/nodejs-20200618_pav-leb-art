e81f05bde9125118ef3864729205494137d139a0
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-06-29 16:06:45 +0300

    WIP solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,23 +1,96 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const stream = require('stream');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+	const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+	const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'POST':
+	if (path.dirname(pathname) !== '.') {
+		res.statusCode = 400;
+		res.end('Nested folders are not supported.');
+	}
 
-      break;
+	switch (req.method) {
+		case 'POST':
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+			const limitStream = new LimitSizeStream({
+				limit: 10 ** 6
+			});
+
+			const writeStream = fs.createWriteStream(filepath, {
+				flags: 'ax'
+			});
+
+			let deleteFlag = false;
+
+			limitStream.on('error', (err) => {
+				res.statusCode = err.code === 'LIMIT_EXCEEDED' ? 413 : 500;
+				deleteFlag = true;
+				res.end()
+			})
+
+			writeStream.on('error', (err) => {
+				res.statusCode = err.code === 'EEXIST' ? 409 : 500;
+				res.end()
+			})
+
+			writeStream.on('close', () => {
+				res.statusCode = 201;
+				res.end()
+			})
+
+			req.pipe(limitStream).pipe(writeStream)
+
+			// ERR_INVALID_ARG_TYPE
+			// stream.pipeline(
+			// 	req,
+			// 	limitStream,
+			// 	writeStream,
+			// 	(err) => {
+			// 		if (err) {
+			// 			switch (err.code) {
+			// 				case 'LIMIT_EXCEEDED':
+			// 					res.statusCode = 413;
+			// 					deleteFlag = true
+			// 					break;
+
+			// 				case 'EEXIST':
+			// 					res.statusCode = 409;
+			// 					break;
+
+			// 				default:
+			// 					res.statusCode = 500;
+			// 					break;
+			// 			}
+			// 		} else {
+			// 			res.statusCode = 201;
+			// 		}
+			// 		res.end();
+			// 	}
+			// )
+
+			res.on('close', () => {
+				if (deleteFlag || !res.finished) {
+					limitStream.destroy();
+					writeStream.destroy();
+					fs.unlink(filepath, (err) => {
+						if (err) console.log(err);
+					})
+				}
+			})
+			break;
+
+		default:
+			res.statusCode = 501;
+			res.end('Not implemented');
+	}
 });
 
 module.exports = server;
