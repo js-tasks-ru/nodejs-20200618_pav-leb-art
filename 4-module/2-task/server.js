f17a8f3fd7c6449f2ef448fd8ec20bceec4f60fc
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-06-30 18:24:01 +0300

    4-modue 2-task fixes

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -15,82 +15,90 @@ server.on('request', (req, res) => {
 	if (path.dirname(pathname) !== '.') {
 		res.statusCode = 400;
 		res.end('Nested folders are not supported.');
+	} else {
+		switch (req.method) {
+			case 'POST':
+	
+				const limitStream = new LimitSizeStream({
+					limit: 10 ** 6,
+				});
+	
+				const writeStream = fs.createWriteStream(filepath, {
+					flags: 'ax',
+				});
+	
+				let deleteFlag = false;
+	
+				limitStream.on('error', (err) => {
+					if(!res.finished) {
+						res.statusCode = err.code === 'LIMIT_EXCEEDED' ? 413 : 500;
+						res.end('File is too big');
+					}
+					deleteFlag = true;
+				})
+	
+				writeStream.on('error', (err) => {
+					if(!res.finished) {
+						res.statusCode = err.code === 'EEXIST' ? 409 : 500;
+						res.end('File already exist');
+					}
+				})
+	
+				writeStream.on('close', () => {		
+					if(!res.finished) {
+						res.statusCode = 201;
+						res.end('Success');
+					}
+				})
+	
+				req.pipe(limitStream).pipe(writeStream);
+	
+				// // ERR_INVALID_ARG_TYPE
+				// stream.pipeline(
+				// 	req,
+				// 	limitStream,
+				// 	writeStream,
+				// 	(err) => {
+				// 		if (err) {
+				// 			switch (err.code) {
+				// 				case 'LIMIT_EXCEEDED':
+				// 					res.statusCode = 413;
+				// 					deleteFlag = true
+				// 					break;
+	
+				// 				case 'EEXIST':
+				// 					res.statusCode = 409;
+				// 					break;
+	
+				// 				default:
+				// 					res.statusCode = 500;
+				// 					break;
+				// 			}
+				// 		} else {
+				// 			res.statusCode = 201;
+				// 		}
+				// 		res.end('Server response');
+				// 	}
+				// )
+	
+				res.on('close', () => {
+					// console.log('res', res.finished);
+					if (deleteFlag || !res.finished) {
+						limitStream.destroy();
+						writeStream.destroy();
+						fs.unlink(filepath, (err) => {
+							if (err) console.log(err);
+						})
+					}
+				})
+				break;
+	
+			default:
+				res.statusCode = 501;
+				res.end('Not implemented');
+		}
 	}
 
-	switch (req.method) {
-		case 'POST':
-
-			const limitStream = new LimitSizeStream({
-				limit: 10 ** 6,
-			});
-
-			const writeStream = fs.createWriteStream(filepath, {
-				flags: 'ax',
-			});
-
-			let deleteFlag = false;
-
-			limitStream.on('error', (err) => {
-				res.statusCode = err.code === 'LIMIT_EXCEEDED' ? 413 : 500;
-				deleteFlag = true;
-				res.end('File is too big');
-			})
-
-			writeStream.on('error', (err) => {
-				res.statusCode = err.code === 'EEXIST' ? 409 : 500;
-				res.end('File already exist');
-			})
-
-			writeStream.on('close', () => {
-				res.statusCode = 201;
-				res.end('Success');
-			})
-
-			req.pipe(limitStream).pipe(writeStream);
-
-			// // ERR_INVALID_ARG_TYPE
-			// stream.pipeline(
-			// 	req,
-			// 	limitStream,
-			// 	writeStream,
-			// 	(err) => {
-			// 		if (err) {
-			// 			switch (err.code) {
-			// 				case 'LIMIT_EXCEEDED':
-			// 					res.statusCode = 413;
-			// 					deleteFlag = true
-			// 					break;
-
-			// 				case 'EEXIST':
-			// 					res.statusCode = 409;
-			// 					break;
-
-			// 				default:
-			// 					res.statusCode = 500;
-			// 					break;
-			// 			}
-			// 		} else {
-			// 			res.statusCode = 201;
-			// 		}
-			// 		res.end('Server response');
-			// 	}
-			// )
-
-			res.on('close', () => {
-				if (deleteFlag || !res.finished) {
-					limitStream.destroy();
-					writeStream.destroy();
-					fs.unlink(filepath, (err) => {
-						if (err) console.log(err);
-					})
-				}
-			})
-			break;
-
-		default:
-			res.statusCode = 501;
-			res.end('Not implemented');
-	}
 });
 
 module.exports = server;
