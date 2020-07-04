4c7e34e87cdbe31e841871d55473ac4cc17e97ab
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-07-04 16:04:47 +0300

    add solution for modue 5 task 1

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,50 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+    const id = ctx.request.query.r;
+    subscribers[id] = {
+        ctx,
+        done: false
+    }
+
+    ctx.res.on('close', () => {
+        delete subscribers[id];
+    });
+
+    await new Promise((resolve, reject) => {
+        const intervalId = setInterval(() => {
+            if(subscribers[id] && subscribers[id].done) {
+                clearInterval(intervalId);
+                resolve();
+            } else if(!subscribers[id]) {
+                clearInterval(intervalId);
+            }
+        }, 300);
+    })
 });
 
 router.post('/publish', async (ctx, next) => {
+    if(!ctx.request.body.message) {
+        ctx.response.status = 204;
+        ctx.response.message = 'Empty message';
+
+        return
+    }   
+
+    for(let id in subscribers) {
+        let subscriberContext = subscribers[id].ctx;
+        
+        subscriberContext.response.status = 200;
+        subscriberContext.body = ctx.request.body.message;
+
+        subscribers[id].done = true;
+    }
+
+    ctx.response.status = 200;
+    ctx.response.message = 'Message published';
 });
 
 app.use(router.routes());
