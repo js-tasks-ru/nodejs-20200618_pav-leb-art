8b25d9976abb85b71208774f322672fcceef0ec1
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-07-05 20:49:38 +0300

    5 module 1 task fix

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,18 +8,11 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-let subscribers = {};
+let subscribers = [];
 
 router.get('/subscribe', async (ctx, next) => {
-    const id = ctx.request.query.r;
-    subscribers[id] = new Object();
-
-    ctx.res.on('close', () => {
-        delete subscribers[id];
-    });
-
     await new Promise((resolve, reject) => {
-        subscribers[id].resolve = resolve;
+        subscribers.push(resolve);
     }).then(message => {
         ctx.response.body = message;
         ctx.status = 200;
@@ -34,10 +27,9 @@ router.post('/publish', async (ctx, next) => {
 
         return
     }   
-
-    for(let id in subscribers) {
-        subscribers[id].resolve(ctx.request.body.message);
-    }
+    subscribers.forEach(resolve => {
+        resolve(ctx.request.body.message)
+    })
 
     ctx.status = 200;
     ctx.message = 'Message published';
