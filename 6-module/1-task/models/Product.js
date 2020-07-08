4fbb60751a04e7edec71b8f03faf7f3cfc62db70
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-07-09 01:24:54 +0300

    6-module 1-task solution

--- 6-module/1-task/models/Product.js
+++ 6-module/1-task/models/Product.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
+const { isInteger } = require('lodash');
 
 const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    subcategory: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    images: [String]
+
 });
 
 module.exports = connection.model('Product', productSchema);
