5a5170340fd3be07a606f823b923ba6124b3910f
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-07-14 22:13:22 +0300

    7 module 2 task solution

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,26 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  // console.log(arguments)
+  if(!email) {
+    return done(null, false, `Не указан email`)
+  };
+
+  const user = await User.findOne({ email });
+  
+  if(!user) {
+    const newUser = new User({
+      email,
+      displayName
+    });
+
+    const error = newUser.validateSync();
+    if(error) return done(error);
+
+    await newUser.save()
+
+    return done(null, newUser);
+  };
+
+  done(null, user);
 };
