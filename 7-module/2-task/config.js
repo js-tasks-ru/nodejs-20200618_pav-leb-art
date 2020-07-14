008867f0c1d013abce2d6ff8e0ac58f543ce3fd0
Лебедев Павел Артурович <lebedev_pa@bdo.ru>
2020-07-14 22:23:27 +0300

    7 module 2 task fix

--- 7-module/2-task/config.js
+++ 7-module/2-task/config.js
@@ -11,8 +11,8 @@ module.exports = {
   },
   providers: {
     github: {
-      app_id: process.env.GITHUB_APP_ID || 'github_app_id',
-      app_secret: process.env.GITHUB_APP_SECRET || 'github_app_secret',
+      app_id: process.env.GITHUB_APP_ID || '77f9d5498fddf09331f3',
+      app_secret: process.env.GITHUB_APP_SECRET || '303b1c6f475686cbbe2d214f33185cdcdb28d4e4',
       callback_uri: 'http://localhost:3000/oauth/github',
       options: {
         scope: ['user:email'],
